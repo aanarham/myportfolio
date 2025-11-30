aa47de73f5f87f875fa1e5e798277b7aaa080a85
aanarham <aan.croco@gmail.com>
2025-11-30 14:03:29 +0800

    Initial commit

---
+++ blog.php
@@ -0,0 +1,20 @@
+<?php
+$pageTitle = 'Blog - Arham Nugraha';
+$currentPage = 'blog';
+include 'includes/header.php';
+?>
+<div class="page-content">
+    <h1 class="page-title">Blog</h1>
+    
+    <div class="blog-content">
+        <p>Blog tentang pengalaman kerja, tips, dan sharing pengetahuan seputar dunia sales marketing dan distribusi.</p>
+        
+        <div class="blog-posts">
+            <div class="blog-post">
+                <h3>Tips Sukses di Bidang Sales Marketing</h3>
+                <p class="blog-date">Jangan Tanya saya,saya juga tidak tau</p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'includes/footer.php'; ?>
